622e1b94f4d675b15772688576d9b89437361c55
Vanessa Rodrigues <th3.van89@gmail.com>
2025-12-16 17:13:23 +0000

    finished screen drawing functionality

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "string.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -45,7 +46,8 @@ I2C_HandleTypeDef hi2c1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-
+#define DISPLAY_X_SIZE 128
+#define DISPLAY_Y_SIZE 64
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -53,8 +55,10 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_I2C1_Init(void);
-static void I2C_SSD1306_Screen_Init(void);
 /* USER CODE BEGIN PFP */
+void I2C_SSD1306_Screen_Init(void);
+void I2C_SSD1306_Update_Whole_Display(uint8_t SSD1306_FrameBufferPages[128][8]);
+static void I2C_SSD1306_Screen_Transmit(uint16_t data_len, uint8_t * data);
 
 /* USER CODE END PFP */
 
@@ -80,6 +84,9 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
+  //a buffer of the current view on the screen
+  //each bit corresponds to a VERTICAL section of 8 (single bit) pixels
+  uint8_t SSD1306_FrameBufferPages[128][8] = {{}};
 
   /* USER CODE END Init */
 
@@ -97,12 +104,6 @@ int main(void)
   /* USER CODE BEGIN 2 */
   I2C_SSD1306_Screen_Init();
 
-  void screen_transmit(uint8_t data_len, uint8_t * data){
-	  HAL_I2C_Master_Transmit(&hi2c1, ((uint16_t)0x3c)<<1, data, data_len, 1000);
-  }
-
-  HAL_Delay(100);
-
 
 
 
@@ -117,7 +118,14 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 
-	  HAL_Delay(100);
+	  memset(SSD1306_FrameBufferPages, 0b00000000, 1024);
+	  HAL_Delay(1000);
+	  I2C_SSD1306_Update_Whole_Display(SSD1306_FrameBufferPages);
+
+	  memset(SSD1306_FrameBufferPages, 0b11111111, 1024);
+	  HAL_Delay(1000);
+	  I2C_SSD1306_Update_Whole_Display(SSD1306_FrameBufferPages);
+	  continue;
 
   }
   /* USER CODE END 3 */
@@ -185,7 +193,7 @@ static void MX_I2C1_Init(void)
 
   /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.ClockSpeed = 400000;
   hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
   hi2c1.Init.OwnAddress1 = 128;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
@@ -275,103 +283,174 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-static void I2C_SSD1306_Screen_Init(void){
-	void screen_transmit(uint8_t data_len, uint8_t * data){
-	  HAL_I2C_Master_Transmit(&hi2c1, ((uint16_t)0x3c)<<1, data, data_len, 1000);
-	}
+
+/**
+  * @brief I2C initilisation of a small OLED screen
+  * @param None
+  * @retval None
+  */
+void I2C_SSD1306_Screen_Init(void){
 
 	uint8_t data[2];
 
 	// Set the MUX ratio (0xA8) to 0x3F
 	data[0] = 0x80;
 	data[1] = 0xA8;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 	data[0] = 0x80;
 	data[1] = 0x3F;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 
 	// Set the Display Offset (0xD3) to 0x00
 	data[0] = 0x80;
 	data[1] = 0xD3;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 	data[0] = 0x80;
 	data[1] = 0x00;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 
 	// Set Display Start Line (0x40)
 	data[0] = 0x80;
 	data[1] = 0x40;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 
 	// Set Segment re-map (0xA1)
 	data[0] = 0x80;
 	data[1] = 0xA1;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 
-	 // Set COM Scan Direction (0xC8)
+	 // I2C_SSD1306_Screen_Transmit(ection (0xC8)
 	data[0] = 0x80;
 	data[1] = 0xC8;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 
 	 // Set COM hardware configuration (0xDA) to 0x12
 	data[0] = 0x80;
 	data[1] = 0xDA;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 	data[0] = 0x80;
 	data[1] = 0x12;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 
 	// Set Contrast Control (0x81) to 0x7F
 	data[0] = 0x80;
 	data[1] = 0x81;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 	data[0] = 0x80;
 	data[1] = 0x7F;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 
 	// Set Disable Entire Display On (0xA4)
 	data[0] = 0x80;
 	data[1] = 0xA4;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 
 	// Set Normal Display (0xA6)
 	data[0] = 0x80;
 	data[1] = 0xA6;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 
 	// Set OSC Frequency (0xD5) to 0x80
 	data[0] = 0x80;
 	data[1] = 0xD5;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 	data[0] = 0x80;
 	data[1] = 0x80;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 
 	// Set Enable Charge Pump Regulator (0xAD) to 0x14
 	data[0] = 0x80;
 	data[1] = 0x8D;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 	data[0] = 0x80;
 	data[1] = 0x14;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 
 
 	// Set Display On (0xAF)
 	data[0] = 0x80;
 	data[1] = 0xAF;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 
 
 	 // Set Direction for pages (0x20) to columns (0x01)
 	data[0] = 0x80;
 	data[1] = 0x20;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
 	data[0] = 0x80;
 	data[1] = 0x01;
-	screen_transmit(2, data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)2, data);
+
+//	uint8_t command_data[13] = {0x80, 0x21, 0x80, 0x0, 0x80, 127, 0x80, 0x22, 0x80, 0x00, 0x80, 0x07};
+//	I2C_SSD1306_Screen_Transmit(13, &command_data);
+//	HAL_Delay(1000);
+  
+//	 while(1){
+//	 	uint8_t data[2] = {0x40, 0b00000000};
+//	 	I2C_SSD1306_Screen_Transmit(2, data);
+//	 }
 
 }
 
+/**
+  * @brief transmission of data over I2C to OLED screen
+  * @param data_len number of frames to transmit, data actual data to transmit
+  * @retval None
+  */
+
+void I2C_SSD1306_Update_Whole_Display(uint8_t SSD1306_FrameBufferPages[128][8]){
+	 // Set the columns and pages to be written to
+
+	// 0x80 - Control byte
+	// 0x21 - Set Column
+	// 0x80 - Control byte
+	// 0x0 - Set starting column to 0
+	// 0x80 - Control byte
+	// 127 - Set end column to 127
+	// ---------------------------
+	// 0x80 - Control byte
+	// 0x22 - Set page
+	// 0x80 - Control byte
+	// 0x0 - Set starting page to 0
+	// 0x80 - Control byte
+	// 0x7 - Set end page to 7
+
+	uint8_t command_data[12] = {0x80, 0x21, 0x80, 0x0, 0x80, 127, 0x80, 0x22, 0x80, 0x00, 0x80, 0x07};
+
+
+	// Create an array to hold the data to send to the display + 1 (byte to indicate data transmission, which is 0x40)
+	uint8_t data[1025] = { 0 };
+	data[0] = 0x40;
+
+
+	// For each of the columns 0 to 127
+	for (volatile uint8_t x = 0; x < 128; x++)
+	{
+		// For each of the pages, 0 to 7
+		for (volatile uint8_t y = 0; y < 8; y++)
+		{
+			//retrieve the current page from the 2d array, and write that into the 1d array for transmission
+			uint8_t current_page = SSD1306_FrameBufferPages[x][y];
+
+			//8*128 + 1 (all max values) = 1025
+			//we skip the first byte as that is 0x40, to declare data transmission
+			data[((y * 128) + x) + 1] = (uint8_t)current_page;
+		}
+	}
+
+
+	I2C_SSD1306_Screen_Transmit((uint16_t)12, command_data);
+	I2C_SSD1306_Screen_Transmit((uint16_t)1025, data);
+
+
+
+	return;
+}
+
+
+static void I2C_SSD1306_Screen_Transmit(uint16_t data_len, uint8_t * data){
+	  HAL_I2C_Master_Transmit(&hi2c1, ((uint16_t)0x3c)<<1, data, data_len, 100);
+}
 /* USER CODE END 4 */
 
 /**
